94eac07ac9c00f4c39e8f64e03fb9b846f5f047c
VikeMK <62164219+VikeMK@users.noreply.github.com>
2021-04-27 09:09:32 +1000

    Add exception handler to player stat cache warming

--- src/Lounge.Web/Stats/PlayerStatCacheWarmingBackgroundService.cs
+++ src/Lounge.Web/Stats/PlayerStatCacheWarmingBackgroundService.cs
@@ -1,6 +1,6 @@
-﻿using Lounge.Web.Data;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Threading;
 using System.Threading.Tasks;
@@ -11,22 +11,31 @@ namespace Lounge.Web.Stats
     {
         private readonly IServiceProvider _services;
         private readonly IPlayerStatCache _cache;
+        private readonly ILogger<PlayerStatCacheWarmingBackgroundService> _logger;
 
-        public PlayerStatCacheWarmingBackgroundService(IServiceProvider services, IPlayerStatCache cache)
+        public PlayerStatCacheWarmingBackgroundService(IServiceProvider services, IPlayerStatCache cache, ILogger<PlayerStatCacheWarmingBackgroundService> logger)
         {
             _services = services;
             _cache = cache;
+            _logger = logger;
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             while (!stoppingToken.IsCancellationRequested)
             {
-                using (var scope = _services.CreateScope())
+                try
                 {
-                    var playerStatService = scope.ServiceProvider.GetRequiredService<IPlayerStatService>();
-                    var stats = await playerStatService.GetAllStatsAsync();
-                    _cache.UpdateAllPlayerStats(stats);
+                    using (var scope = _services.CreateScope())
+                    {
+                        var playerStatService = scope.ServiceProvider.GetRequiredService<IPlayerStatService>();
+                        var stats = await playerStatService.GetAllStatsAsync();
+                        _cache.UpdateAllPlayerStats(stats);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError(ex, "Exception thrown when updating player stats");
                 }
 
                 await Task.Delay(TimeSpan.FromMinutes(1), stoppingToken);
