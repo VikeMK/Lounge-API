5fce560e0bd212cf11a527edccaba303ca6ad551
VikeMK <62164219+VikeMK@users.noreply.github.com>
2021-06-14 22:20:26 +1000

    Add secondary password support

--- src/Lounge.Web/Authentication/BasicAuthenticationHandler.cs
+++ src/Lounge.Web/Authentication/BasicAuthenticationHandler.cs
@@ -15,11 +15,13 @@ namespace Lounge.Web.Authentication
     {
         private readonly string apiUsername;
         private readonly string apiPassword;
+        private readonly string apiSecondaryPassword;
 
         public BasicAuthenticationHandler(IOptionsMonitor<AuthenticationSchemeOptions> options, ILoggerFactory logger, UrlEncoder encoder, ISystemClock clock, IConfiguration configuration) : base(options, logger, encoder, clock)
         {
             apiUsername = configuration["APICredentials:Username"];
             apiPassword = configuration["APICredentials:Password"];
+            apiSecondaryPassword = configuration["APICredentials:PasswordSecondary"];
         }
 
         protected override Task<AuthenticateResult> HandleAuthenticateAsync()
@@ -37,7 +39,12 @@ namespace Lounge.Web.Authentication
                 var username = credentials[0];
                 var password = credentials[1];
 
-                if (username == this.apiUsername && password == this.apiPassword)
+                var usernameMatch = string.Equals(username, this.apiUsername, StringComparison.Ordinal);
+                var primaryPasswordMatch = !string.IsNullOrWhiteSpace(this.apiPassword) && string.Equals(password, this.apiPassword, StringComparison.Ordinal);
+                var secondaryPasswordMatch = !string.IsNullOrWhiteSpace(this.apiSecondaryPassword) && string.Equals(password, this.apiSecondaryPassword, StringComparison.Ordinal);
+                var passwordMatch = primaryPasswordMatch || secondaryPasswordMatch;
+
+                if (usernameMatch && passwordMatch)
                 {
                     var claims = new[] { new Claim(ClaimTypes.NameIdentifier, username) };
                     var identity = new ClaimsIdentity(claims, Scheme.Name);
