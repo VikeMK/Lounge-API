5c415905354645a64b36cd4ac3d96ac1f60d4328
VikeMK <62164219+VikeMK@users.noreply.github.com>
2021-08-15 12:10:08 +1000

    S5 changes

---
+++ src/Lounge.Web/Migrations/20210814231614_NewDefaultSeason.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Lounge.Web.Migrations
+{
+    public partial class NewDefaultSeason : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Season",
+                table: "Tables",
+                type: "int",
+                nullable: false,
+                defaultValue: 5,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldDefaultValue: 4);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Season",
+                table: "PlayerSeasonData",
+                type: "int",
+                nullable: false,
+                defaultValue: 5,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldDefaultValue: 4);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Season",
+                table: "Placements",
+                type: "int",
+                nullable: false,
+                defaultValue: 5,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldDefaultValue: 4);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Season",
+                table: "Penalties",
+                type: "int",
+                nullable: false,
+                defaultValue: 5,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldDefaultValue: 4);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Season",
+                table: "Bonuses",
+                type: "int",
+                nullable: false,
+                defaultValue: 5,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldDefaultValue: 4);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Season",
+                table: "Tables",
+                type: "int",
+                nullable: false,
+                defaultValue: 4,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldDefaultValue: 5);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Season",
+                table: "PlayerSeasonData",
+                type: "int",
+                nullable: false,
+                defaultValue: 4,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldDefaultValue: 5);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Season",
+                table: "Placements",
+                type: "int",
+                nullable: false,
+                defaultValue: 4,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldDefaultValue: 5);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Season",
+                table: "Penalties",
+                type: "int",
+                nullable: false,
+                defaultValue: 4,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldDefaultValue: 5);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Season",
+                table: "Bonuses",
+                type: "int",
+                nullable: false,
+                defaultValue: 4,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldDefaultValue: 5);
+        }
+    }
+}
