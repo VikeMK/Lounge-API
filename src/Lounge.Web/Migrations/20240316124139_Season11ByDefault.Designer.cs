12ec0fb7e7cc3cfb8c9dd2b4ec57a8afd0ec182b
Vike <62164219+VikeMK@users.noreply.github.com>
2024-03-16 22:54:13 +1000

    Season 11 (#46)

--- src/Lounge.Web/Migrations/20240316124139_Season11ByDefault.Designer.cs
+++ src/Lounge.Web/Migrations/20240316124139_Season11ByDefault.Designer.cs
@@ -4,6 +4,7 @@ using Lounge.Web.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Lounge.Web.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240316124139_Season11ByDefault")]
+    partial class Season11ByDefault
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
